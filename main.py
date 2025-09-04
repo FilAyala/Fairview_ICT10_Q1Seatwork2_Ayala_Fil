447359a0585d2ae9f62778cb0a55c10663e9ab52
FilAyala <Filclaren@gmail.com>
2025-09-04 11:31:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from pyscript import display
+
+store_name = "The standard"
+#this is a string that tells us what is the name of the store
+
+owner_name = "Fil Ayala"
+#this is a string that tells us what is the name of the owner who owns the store
+
+year_established = 2021
+#this is an integer that tells us what year was the store established
+
+popular_item = "Standard Wilson Basketball"
+#this is a string that tells us what is the popular item of the store
+
+has_delivery = True
+#this is a boolean that tells us if delivery is available for the store
+
+product_names = ['Standard Wilson Basketball', 'Kids basketball hoop', 'Standard height adjustable basketball stand', 'Sport socks', 'Waterbotlle']
+#this is a list that tells us the names of the products
+
+business_hours = "9 AM - 9 PM"
+#this is a string that tells us the business hours of the store
+
+product_prices = {'Standard Wilson Basketball':'₱2,236', 'Kids basketball hoop':'₱6,990', 'Standard height adjustable basketball hoop':'₱19,900', 'Sport socks':'₱250', 'Waterbottle':'₱460'}
+#this is a dictionary that tells us what are the prices of the products
+
+common_injuries = ['Ankle sprain', 'Leg cramps', 'Sprained wrist', 'Jammed finger']
+#this is a list that tells us the common injuries of playing basketball
+
+tax_rate = 0.08
+#this is a float that tells us the tax rate of the items when you purchase them
+
+display(store_name)
+
+display(owner_name)
+
+display(year_established)
+
+display(popular_item)
+
+display(has_delivery)
+
+display(product_names)
+
+display(business_hours)
+
+display(product_prices)
+
+display(common_injuries)
+
+display(tax_rate)
